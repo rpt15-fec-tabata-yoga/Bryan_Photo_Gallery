63271920189aa3479c0372848644e183e9a14c8a
Bryan Nguyen <bryan.m.nguyen12@gmail.com>
2019-09-16 18:35:20 -0700

    Added cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Images = require('../database/Image.js');
+const cors = require('cors');
 
 const app = express();
 
